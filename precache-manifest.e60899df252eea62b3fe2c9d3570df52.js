ffc84205aa8b10ed748651907fa8e16473d21bd2
Garrett Reichman <gsreichman@gmail.com>
2020-04-25 10:35:38 -0400

    Updates

---
+++ precache-manifest.e60899df252eea62b3fe2c9d3570df52.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a96dac2912e4321b2e3440985b76b43",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "9bb93b9e8f112bfa11ac",
+    "url": "/react-portfolio/static/js/2.3fbce383.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.3fbce383.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b36d7e06bbe5b0c3fa3c",
+    "url": "/react-portfolio/static/js/main.33bdcae8.chunk.js"
+  },
+  {
+    "revision": "c01136dc6b7e393f307b",
+    "url": "/react-portfolio/static/js/runtime-main.d92c7dfa.js"
+  },
+  {
+    "revision": "f433b6787b185e7e0026149c0908d12f",
+    "url": "/react-portfolio/static/media/archer.f433b678.jpg"
+  },
+  {
+    "revision": "71063961ed22f50a78d8fdcba7412e19",
+    "url": "/react-portfolio/static/media/burger.71063961.png"
+  },
+  {
+    "revision": "f57d5a43923a06f010b1c9b3f9f521aa",
+    "url": "/react-portfolio/static/media/home.f57d5a43.jpg"
+  },
+  {
+    "revision": "04c54f467aa5bd5f2f221d4c4eddb442",
+    "url": "/react-portfolio/static/media/photo2.04c54f46.png"
+  },
+  {
+    "revision": "2e3189d4b618a56dbd7b29ed72d9d988",
+    "url": "/react-portfolio/static/media/planner.2e3189d4.png"
+  },
+  {
+    "revision": "a6ed5402732604b5ef286c13682fd4e4",
+    "url": "/react-portfolio/static/media/resume.a6ed5402.pdf"
+  },
+  {
+    "revision": "0d2f94331eceb381bde3265e000cea92",
+    "url": "/react-portfolio/static/media/weather.0d2f9433.png"
+  }
+]);
